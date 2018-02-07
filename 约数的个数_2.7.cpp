1bfbc245fbfa83889abb217f8a2d1d4a9095ede2
lsx88 <32868153+lsx88@users.noreply.github.com>
2018-02-07 14:38:36 +0800

    Update 约数的个数_2.7.cpp

--- 约数的个数_2.7.cpp
+++ 约数的个数_2.7.cpp
@@ -1,7 +1,16 @@
-//�����Ӧ����˵�Ƚϼ򵥣�������һ����Ҫע�⣬��������Լ����ѭ����ʱ��Ӧ��ѭ������n�ζ�����n/2�Σ��������Ա���ܶ���ν�����㣬 
-//����С���㷨���Ӷ��������ȷ�Ĵ𰸲�������Ҫ���ģ������ǽ�����һ���� 
-//�ο����� https://zhidao.baidu.com/question/565505959255455924.html
-//��Ŀ���� https://www.nowcoder.net/practice/04c8a5ea209d41798d23b59f053fa4d6 
+/*
+*作者：廉思夏
+*2018.2.7
+*约数的个数 
+*/
+
+/* 
+*这道题应该来说比较简单，但是有一点需要注意，就是在求约数，循环的时候应该循环根号n次而不是n/2次，这样可以避免很多无谓的运算， 
+*以最小的算法复杂度来求出正确的答案才是我们要做的，而不是仅仅求一个答案 
+*参考链接 https://zhidao.baidu.com/question/565505959255455924.html
+*题目链接 https://www.nowcoder.net/practice/04c8a5ea209d41798d23b59f053fa4d6 
+*/ 
+
 #include<iostream>
 using namespace std;
 void submultiple(int n){
