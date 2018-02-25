f1159009ea99c79926cfb905bb2cf2ce40fccdca
Qingsong Lv <lqs@mail.bnu.edu.cn>
2018-02-25 16:05:21 +0800

    Update 二叉树遍历_2.25.cpp

--- 二叉树遍历_2.25.cpp
+++ 二叉树遍历_2.25.cpp
@@ -12,16 +12,16 @@
 using namespace std;
 
 char s[105];
-int l[105], r[105];
+//int l[105], r[105];
 
 pair<int, int> dfs(int u)  // 返回从s[u]开始的子树的根，和这个子树最右端到输入串的哪里
 {
     if (s[u]=='#') return make_pair(-1, u);
     pair<int, int> left = dfs(u+1);
-    printf("%c ", s[u]);
+    printf("%c ", s[u]);  // 写完这个函数以后发现中序遍历直接在这里就可以做了，所以记录的l和r也就没有什么作用了。
     pair<int, int> right = dfs(left.second+1);
-    l[u] = left.first;
-    r[u] = right.first;
+    //l[u] = left.first;
+    //r[u] = right.first;
     return make_pair(u, right.second);
 }
 
